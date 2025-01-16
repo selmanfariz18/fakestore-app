8bdf99c50af729afa2dfaf74b6e53354547ef0ad
selmanfariz18 <salmanulfarizyka@gmail.com>
2025-01-16 19:05:12 +0530

    landing page ready

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -10,23 +10,71 @@ class LandingPage extends StatelessWidget {
         children: [
           // Middle images
           Positioned(
-            top: 100,
-            left: 50,
-            child:
-                Image.asset('assets/images/image1.png', width: 50, height: 50),
+            top: -20,
+            left: -203,
+            child: Container(
+              child: Image.asset(
+                'assets/images/Pattern.png',
+                width: 810.64,
+                height: 859.55,
+              ),
+            ),
           ),
           Positioned(
-            top: 200,
-            right: 70,
-            child:
-                Image.asset('assets/images/image2.png', width: 40, height: 40),
+            top: 180,
+            left: 233,
+            child: Image.asset('assets/images/image1.png',
+                width: 86.46, height: 79.96),
           ),
           Positioned(
-            bottom: 300,
-            left: 20,
-            child:
-                Image.asset('assets/images/image3.png', width: 60, height: 60),
+            top: 175,
+            left: 72,
+            child: Image.asset('assets/images/image2.png',
+                width: 79.34, height: 71.75),
           ),
+          Positioned(
+            top: 238,
+            left: 61.75,
+            child: Image.asset('assets/images/image3.png',
+                width: 10.75, height: 14.36),
+          ),
+          Positioned(
+            top: 163,
+            left: 33.89,
+            child: Image.asset('assets/images/Illustration.png',
+                width: 311.11, height: 310.56),
+          ),
+          Positioned(
+            top: 291,
+            left: 211,
+            child: Image.asset('assets/images/Group.png',
+                width: 47.8, height: 47.8),
+          ),
+          Positioned(
+            top: 285,
+            left: 110.59,
+            child: Image.asset('assets/images/Group1.png',
+                width: 47.78, height: 42.31),
+          ),
+          Positioned(
+            top: 308,
+            left: 104,
+            child: Image.asset('assets/images/Group1.png',
+                width: 47.78, height: 42.31),
+          ),
+          Positioned(
+            top: 376,
+            left: 67,
+            child: Image.asset('assets/images/Group2.png',
+                width: 87.22, height: 90),
+          ),
+          Positioned(
+            top: 353,
+            left: 207,
+            child: Image.asset('assets/images/Group3.png',
+                width: 129.71, height: 103),
+          ),
+
           // "Later" button
           Positioned(
             top: 40,
@@ -43,7 +91,8 @@ class LandingPage extends StatelessWidget {
               ),
             ),
           ),
-          // Login and Create Account buttons
+
+          // Login and Create Account buttons with the text above them
           Align(
             alignment: Alignment.bottomCenter,
             child: Padding(
@@ -51,28 +100,73 @@ class LandingPage extends StatelessWidget {
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/login');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.teal,
-                      backgroundColor: Colors.white,
-                      minimumSize: const Size(200, 50),
+                  // Text above the login button
+                  const Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Text(
+                      "Help your path to health goals with happiness",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Sofia Pro',
+                        fontWeight: FontWeight.w800,
+                        fontSize: 28,
+                        height: 36.4 / 28, // Line height / font size
+                        color: Colors.white,
+                      ),
                     ),
-                    child: const Text('Login'),
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: 327,
+                    height: 54,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF042628),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/login');
+                      },
+                      style: TextButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 16,
+                            horizontal: 40), // Reduced horizontal padding
+                      ),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
                   ),
                   const SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/signup');
-                    },
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.white,
-                      backgroundColor: Colors.teal,
-                      minimumSize: const Size(200, 50),
+                  Container(
+                    width: 327,
+                    height: 54,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
                     ),
-                    child: const Text('Create a New Account'),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                      ),
+                      child: const Text(
+                        'Create a New Account',
+                        textAlign: TextAlign.center, // Correctly placed here
+                        style: TextStyle(
+                          fontFamily: 'Sofia Pro',
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                          height: 21.6 / 16, // Line height / font size
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               ),
